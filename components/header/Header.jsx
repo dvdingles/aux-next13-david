fdca787a91fb615c4d474874b4472ca891247bf2
David Inglés <dvdingles@hotmail.com>
2022-12-12 22:30:00 +0100

    con links

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,14 +1,43 @@
+import Link from 'next/link'
 import styles from './header.module.css'
+
+
+const menus = [{
+    label: 'Inicio',
+    route: '/'
+},{
+    label: 'Productos',
+    route: '/productos'
+},{
+    label: 'Contacto',
+    route: '/contacto'
+},{
+    label: 'Nostros',
+    route: '/nosotros'
+}]
+
+
 export default function Header(){
+
+
+
+
 
     return(
         <header className={styles.header}>
             <nav>
                 <ul>
-                    <li>Inicio</li>
+                    {menus.map(( {label, route }) => (
+                        <li key={route}>
+                            <Link href={route}>
+                                {label}
+                            </Link>
+                        </ li>
+                    ))}
+                    {/* <li>Inicio</li>
                     <li>Productos</li>
                     <li>Contacto</li>
-                    <li>Nostros</li>
+                    <li>Nostros</li> */}
                 </ul>
             </nav>
         </header>
