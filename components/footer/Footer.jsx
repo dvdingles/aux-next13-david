3710a1b64c48b01e293b4e0000f249c1eaa623f8
David Inglés <dvdingles@hotmail.com>
2022-12-12 18:19:37 +0100

    hago un reset2

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+import styles from './footer.module.css'
+
+export default function Footer() {
+  return (
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+        </a>
+      </footer>
+  )
+}
